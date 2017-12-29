042591e778a5cc455b6a00dd9ee8ffc75221d849
pleycpl <h4rvey@protonmail.com>
2017-12-29 17:33:23 +0000

    init project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace asiye {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello Universe");
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+
+            Console.ReadKey();
+            driver.Close();
+
+            Console.WriteLine("Okey!");
+        }
+    }
+}
