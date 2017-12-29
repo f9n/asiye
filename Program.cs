ec94dc83b6d90f351e35fe29af8919c19eb274af
pleycpl <h4rvey@protonmail.com>
2017-12-29 18:52:02 +0000

    added LoginYoutube(), GetChannelLinks(), PrintChannelLinks() functions

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -8,12 +10,50 @@ namespace asiye {
             Console.WriteLine("Hello Universe");
 
             IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("http://www.google.com");
+
+            LoginYoutube(driver);
+
+            List<string> channels = GetChannelLinks(driver);
+
+            PrintChannelLinks(channels);
 
             Console.ReadKey();
             driver.Close();
 
             Console.WriteLine("Okey!");
+        }
+
+        static void LoginYoutube(IWebDriver driver) {
+            string message = "Please Login to Youtube, then press any key on console.";
+
+            driver.Navigate().GoToUrl("https://www.youtube.com");
+
+            Console.WriteLine(message);
+            IJavaScriptExecutor js = driver as IJavaScriptExecutor;
+            js.ExecuteScript($"alert('{message}');");
+
+            Console.ReadKey();
+        }
+
+        static List<string> GetChannelLinks(IWebDriver driver) {
+            string channels_url = "https://www.youtube.com/feed/channels";
+            string classnames = "yt-simple-endpoint style-scope ytd-channel-renderer";
+            List<string> ChannelLinks = new List<string>();
+
+            driver.Navigate().GoToUrl(channels_url);
+            string temp;
+            foreach(var item in driver.FindElements(By.XPath($"//a[contains(@class, '{classnames}')]"))) {
+                temp = item.GetAttribute("href");
+                ChannelLinks.Add(temp);
+            }
+            Console.WriteLine(ChannelLinks.Count);
+            return ChannelLinks;
+        }
+
+        static void PrintChannelLinks(List<string> ChannelLinks) {
+            foreach(var link in ChannelLinks) {
+                Console.WriteLine(link);
+            }
         }
     }
 }
