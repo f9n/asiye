f45930cc96f6c76fab6dff4b22b44bf441f8d4fb
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2018-01-06 00:14:22 +0300

    Clean Code #3 Fonsiyonlar klasörlere alındı.

---
+++ Models/Channel.cs
@@ -0,0 +1,13 @@
+﻿namespace asiye.Models
+{
+    class Channel
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public Channel(string name, string url)
+        {
+            Name = name;
+            Url = url;
+        }
+    }
+}
